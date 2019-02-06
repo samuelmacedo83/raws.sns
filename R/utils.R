65bbff22e0d712dc7388ed991eccb2c24979714e
samuelmacedo83 <samuelmacedo@recife.ifpe.edu.br>
2019-02-06 15:36:03 -0300

    refactor topic_arn()

--- R/utils.R
+++ R/utils.R
@@ -1,19 +1,12 @@
-# this function verifies the topic existence. it will return the
-# arn indepently if topic is a name or arn.
+# it will return the arn indepently if topic is a name or arn.
 topic_arn <- function(topic, profile = "default"){
-
-  topic_list <- list_topics(profile = profile)
 
   # An arn is a string with 6 components separate by : and
   # the first one is always arn.
   arn <- stringr::str_split_fixed(topic,":", 6)[ , 1]
 
-  if (arn == "arn"){
-    topic_exist <- topic_list$arn == topic
-    if (!any(topic_exist)){
-      stop("This topic does not exist.")
-    }
-  } else {
+  if (arn != "arn"){
+    topic_list <- list_topics(profile = profile)
     topic_exist <- topic_list$topic == topic
 
     if (any(topic_exist)){
@@ -39,8 +32,11 @@ is_valid_name <- function(topic_name, display_name){
                 "hyphens (-), or underscores (_)."))
   }
 
-  if (!is.null(display_name) & stringr::str_length(display_name) > 10)
-    stop("Display name cannot be greater than 10.")
+  if (!is.null(display_name)){
+    if(stringr::str_length(display_name) > 10){
+      stop("Display name cannot be greater than 10.")
+    }
+  }
 }
 
 sns_parameter <- function(command, parameter, value){
@@ -58,4 +54,7 @@ sns_text_parameter <- function(command, parameter, value){
   command
 }
 
-
+profile_region <- function(profile = "default"){
+  prof <- raws.profile::profile_settings(profile)
+  prof$value[prof$name == "region"]
+}
