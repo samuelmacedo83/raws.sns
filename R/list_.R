baf9cad12d624da2f7efa91262bfe0755d719663
samuelmacedo83 <samuelmacedo@recife.ifpe.edu.br>
2019-02-06 15:26:42 -0300

    Put list_topics and list subscrption in one file

--- R/list_.R
+++ R/list_.R
@@ -1,11 +1,33 @@
+#' List your topics and subscriptions
+#'
+#' The \code{list_topics()} shows the topics created in your SNS
+#' account. You can see your subscriptions using
+#'  \code{list_subscriptions("your topic")}.
+#'
+#' @template roxlate-topic
+#' @template roxlate-profile
+#'
+#' @examples
+#' \dontrun{
+#' # list the topics in your account
+#' list_topics()
+#'
+#' # list the subscriptions in a topic using a profile
+#' list_subscriptions(topic = "your topic",
+#'                    profile = "your profile")
+#'
+#' }
+#' @name sns_lists
+NULL
+
+#' @rdname sns_lists
 #' @export
 list_topics <- function(profile = "default"){
 
   topics <- sns("list-topics",
-                profile = profile,
-                system_return = TRUE)
+                profile = profile)
 
-  arn <- jsonlite::fromJSON(topics)$Topics$TopicArn
+  arn <- topics$Topics$TopicArn
 
   # The topic name is in 6th position
   topics_name <- stringr::str_split_fixed(arn,":", 6)[,6]
@@ -14,26 +36,26 @@ list_topics <- function(profile = "default"){
                      arn = arn)
 }
 
-
-# list-subscriptions
+#' @rdname sns_lists
 #' @export
 list_subscriptions <- function(topic,
                                profile = "default"){
 
   subscriptions <- sns("list-subscriptions-by-topic",
                        arn = topic_arn(topic, profile),
-                       profile = profile,
-                       system_return = TRUE)
+                       profile = profile)
 
-  subscriptions <- jsonlite::fromJSON(subscriptions)$Subscriptions
+  subscriptions <- subscriptions$Subscriptions
 
-  arn <- purrr::map_chr(subscriptions$SubscriptionArn, function(e){
-    ifelse(e == "PendingConfirmation", "pending", e)
-  })
+  if (length(subscriptions) == 0){
+    message("There aren't subscriptions in this topic." )
+  } else {
+    arn <- purrr::map_chr(subscriptions$SubscriptionArn, function(e){
+      ifelse(e == "PendingConfirmation", "pending", e)
+    })
 
-  tibble::data_frame(subscriber = subscriptions$Endpoint,
-                     protocol = subscriptions$Protocol,
-                     arn = arn)
-
+    tibble::data_frame(endpoint = subscriptions$Endpoint,
+                       protocol = subscriptions$Protocol,
+                       arn = arn)
+  }
 }
-# list-subscriptions-by-topic
